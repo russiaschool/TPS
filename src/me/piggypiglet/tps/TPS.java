62b6942a10dea82dd28e2944325c4584bb9ab4c6
russiaschool <162641582+russiaschool@users.noreply.github.com>
2026-01-30 17:48:31 -0600

    Update TPS.java

--- src/me/piggypiglet/tps/TPS.java
+++ src/me/piggypiglet/tps/TPS.java
@@ -2,31 +2,36 @@ package me.piggypiglet.tps;
 
 import de.Herbystar.TTA.TTA_Methods;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
-
 import org.bukkit.entity.Player;
+
+import java.util.Locale;
 
 public class TPS extends PlaceholderExpansion {
 
     public boolean canRegister() {
         return true;
     }
+
     public String getIdentifier() {
         return "tps";
     }
+
     public String getPlugin() {
         return null;
     }
+
     public String getAuthor() {
         return "PiggyPiglet";
     }
+
     public String getVersion() {
         return "1.0.0";
     }
 
     public String onPlaceholderRequest(Player p, String identifier) {
         if (identifier.equals("tps")) {
-            double tps = (double) Math.round(TTA_Methods.getTPS(100) * 100.0) / 100.0;
-            return String.valueOf(tps);
+            double tps = Math.round(TTA_Methods.getTPS(100) * 100.0) / 100.0;
+            return String.format(Locale.US, "%.2f", tps);
         }
         return null;
     }
